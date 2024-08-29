0c1835c86c6da08295e18df76122aa159ea6a98d
felipe-py <112325208+felipe-py@users.noreply.github.com>
2024-08-28 22:44:08 -0300

    :sparkles: feat: Adicionada conexão entre cliente e servidor

---
+++ src/servidor.py
@@ -0,0 +1,29 @@
+import socket
+import struct
+
+# Criação do socket
+server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+# Vinculando o socket ao endereço e porta
+server_socket.bind(('127.0.0.1', 65432))  # IP local e porta
+
+# Colocando o socket em modo de escuta, pronto para aceitar conexões
+server_socket.listen()
+
+print("Servidor escutando...")
+
+# Aceitando conexões de clientes
+conn, addr = server_socket.accept()
+print(f"Conectado com {addr}")
+
+# Recebendo dados
+data = conn.recv(1024)
+numero_recebido = int(data.decode('utf-8'))
+
+# Respondendo ao cliente
+
+conn.sendall(str(numero_recebido).encode('utf-8'))
+#conn.sendall(struct.pack('!I', numero_recebido))    MAIS EFICIENTE
+
+# Fechando a conexão
+conn.close()
