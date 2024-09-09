44a8dcd5ed90174652f18dd3f2ddb97e94c4385d
felipe-py <112325208+felipe-py@users.noreply.github.com>
2024-09-09 20:01:56 -0300

    Testando métodos relacionados a viagem

--- app/app.py
+++ app/app.py
@@ -1,7 +1,28 @@
-from models.cliente.Cliente import Cliente
+from models.viagem.GerenciadorViagem import GerenciadorViagens
 
-usuarios_cadastrados = Cliente.carregar_clientes()
+'''usuarios_cadastrados = Cliente.carregar_clientes()
 if Cliente.login(2,44,usuarios_cadastrados):
     print("s")
 else:
-    print("n")
+    print("n")'''
+
+geren = GerenciadorViagens()
+geren.diminuir_assentos("1")
+
+print(geren.trechos['1'])
+
+geren.aumentar_assentos("1")
+print(geren.trechos["1"])
+
+geren.adicionar_trecho(5,"Teresina -> Palmas")
+geren.adicionar_trecho(6,"Palmas -> Goiânia")
+geren.adicionar_trecho(7,"Goiânia -> Campo Grande")
+geren.adicionar_trecho(8,"Campo Grande -> São Paulo")
+geren.adicionar_trecho(9,"São Paulo -> Rio de Janeiro")
+
+geren.diminuir_assentos("2")
+print(geren.trechos["2"])
+geren.diminuir_assentos("2")
+print(geren.trechos["2"])
+geren.realizar_viagem("2")
+print(geren.trechos["2"])
